f90739b03be94bd0be3acc74e11de55e092e295b
Md Asif Alam Alam <measifalam@gmail.com>
2023-07-14 19:20:15 +0530

    IActionResult Implementation

---
+++ ControllerAndAction/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllerAndAction.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public string DisplayStr()
+		{
+			return "Asif";
+		}
+
+		public int DisplayInt(int id)
+		{
+			return id;
+		}
+
+		public ContentResult ContentResult()
+		{
+			return Content("A last man who know Every Thing");
+		}
+
+		public JsonResult JsonResult()
+		{
+			var Name = "Md Asif Alam";
+			return Json(new {data=Name});
+		}
+
+		public ViewResult ViewResult()
+		{
+			return View("DisplayStr");
+		}
+
+		public EmptyResult EmptyResult()
+		{
+			return new EmptyResult();
+		}
+	}
+}
