b50f51fa7b4db43591a2bb3590c1efb8aa2c16de
Md Asif Alam Alam <measifalam@gmail.com>
2023-07-18 00:30:49 +0530

    ModelDataWithViewDataBagTemp

---
+++ ModelDataWithViewDataBagTemp/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelDataWithViewDataBagTemp.Models;
+using System.Diagnostics;
+
+namespace ModelDataWithViewDataBagTemp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            //Implementation with Single Obj
+            /*MyEmployee employee = new MyEmployee()
+            {
+               EmpId = 1,
+               EmpName = "Test",
+               Designation="Dev",
+               Salary=123456
+            };*/
+
+            var employee = new List<MyEmployee>
+            {
+               new MyEmployee { EmpId = 1,EmpName="Asif",Designation="Dev",Salary=123456 },
+               new MyEmployee { EmpId = 2,EmpName="Asif",Designation="Dev",Salary=123456 },
+               new MyEmployee { EmpId = 3,EmpName="Asif",Designation="Dev",Salary=123456 },
+               new MyEmployee { EmpId = 4,EmpName="Asif",Designation="Dev",Salary=123456 },
+               new MyEmployee { EmpId = 5,EmpName="Asif",Designation="Dev",Salary=123456 },
+            };
+
+
+
+
+
+
+
+
+           /* ViewData["emp"]= employee;*/
+            /* ViewBag.emp = employee;*/
+            ViewData["emp"] = employee;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ModelDataWithViewDataBagTemp/Models/MyEmployee.cs
@@ -0,0 +1,11 @@
+﻿namespace ModelDataWithViewDataBagTemp.Models
+{
+    public class MyEmployee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+
+        public string Designation { get; set; }
+        public int Salary { get; set; }
+    }
+}
