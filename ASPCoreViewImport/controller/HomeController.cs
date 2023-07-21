45f3094f9b51ecc2f62967e005ac97873322f69b
Md Asif Alam Alam <measifalam@gmail.com>
2023-07-21 23:39:09 +0530

    Implementing ViewImport

---
+++ ASPCoreViewImport/controller/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using ASPCoreViewImport.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPCoreViewImport.controller
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student{Id=1,Name="Asif",Gender="Male"},
+                new Student{Id=1,Name="Alam",Gender="Male"},
+                new Student{Id=1,Name="Asifa",Gender="FeMale"},
+            };
+            return View(students);
+        }
+
+        public IActionResult About()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student{Id=1,Name="Asif",Gender="Male"},
+                new Student{Id=1,Name="Alam",Gender="Male"},
+                new Student{Id=1,Name="Asifa",Gender="FeMale"},
+            };
+            return View(students);
+        }
+
+        public IActionResult Contact()
+        {
+            List<Student> students = new List<Student>
+            {
+                new Student{Id=1,Name="Asif",Gender="Male"},
+                new Student{Id=1,Name="Alam",Gender="Male"},
+                new Student{Id=1,Name="Asifa",Gender="FeMale"},
+            };
+            return View(students);
+        }
+    }
+}
